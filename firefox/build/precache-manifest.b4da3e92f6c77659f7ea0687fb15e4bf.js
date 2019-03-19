f80de4acb2bd81926546a1b6d1b248e3184d1ae0
Snail <snailmounts@163.com>
2019-03-20 00:19:23 +0800

    u

---
+++ firefox/build/precache-manifest.b4da3e92f6c77659f7ea0687fb15e4bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4742d153ee96c68a3005",
+    "url": "/static/js/main.56e904de.chunk.js"
+  },
+  {
+    "revision": "ae6097aa3a636723033a",
+    "url": "/static/js/2.6846ee44.chunk.js"
+  },
+  {
+    "revision": "4742d153ee96c68a3005",
+    "url": "/static/css/main.d5b3d082.chunk.css"
+  },
+  {
+    "revision": "ae6097aa3a636723033a",
+    "url": "/static/css/2.c076df1c.chunk.css"
+  },
+  {
+    "revision": "49c4c179d495671903c93e7d74d210c2",
+    "url": "/index.html"
+  }
+];
